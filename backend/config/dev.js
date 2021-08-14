0c958dbc6fe095e856e44727abcc949cb024c4cd
phpunch <31317741+phpunch@users.noreply.github.com>
2021-08-14 19:16:25 +0700

    Delete dev.js

--- backend/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    googleClientID: '135933121452-ubhauqlsl6lba2f0gdggqpkimt7rg11e.apps.googleusercontent.com',
-    googleClientSecret: 'T05XqDVs5uGIXbXL5AdYWQZb',
-    cookieKey: 'dsofihvinsdivunasovfkjojaisdpovgkdjaspovka',
-    mongoURI: 'mongodb://punch:punch1234@ds219095.mlab.com:19095/chao-ni-car-dev',
-    mongoURITEST: 'mongodb://punch:punch1234@ds023074.mlab.com:23074/cao-ni-car-test',
-    stripeSecretKey: 'sk_test_yUI4QzbYMGoTZRNUafc7hPZW'
-}
